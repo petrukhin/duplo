e62fd16c6cd94d160df483009942639c5ef7eab2
Pavel Petrukhin <petrukhin@yandex-team.ru>
2017-07-23 23:27:47 +0300

    styleguidist

--- src/components/PlayButton/index.jsx
+++ src/components/PlayButton/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './styles.css';
 
-export default function({ hidden }) {
+export default function(props) {
+    const { hidden } = props;
     const className = cx(styles['play-button'], {
         [styles['play-button_hidden']]: hidden
     });
     return <div className={className}>
         <div className={styles['play-button__inner']}/>
     </div>;
-}
+};
