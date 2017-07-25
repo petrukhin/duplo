bbf93e74a1e832dab805e3329d76c2960782f14d
Pavel Petrukhin <petrukhin@yandex-team.ru>
2017-07-26 00:49:59 +0300

    fix

--- .gemini.saucelabs.js
+++ .gemini.saucelabs.js
@@ -2,6 +2,14 @@ module.exports = {
     rootUrl: 'https://premium-jeans.surge.sh',
     gridUrl: 'http://klekh:d7307061-cc43-492f-8d34-302be5b05443@ondemand.saucelabs.com:80/wd/hub',
     screenshotsDir: './gemini/screenshots/',
+    system: {
+        plugins: {
+            'html-reporter': {
+                enabled: true,
+                path: 'gemini-reports'
+            }
+        }
+    },
 
     browsers: {
         Chrome: {
