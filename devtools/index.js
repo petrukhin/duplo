fca484d0caba1e9469fb80c73897241232faa8b9
Konstantin Lekh <klekh@yandex-team.ru>
2017-07-25 19:01:10 +0300

    devtools demo

--- devtools/index.js
+++ devtools/index.js
@@ -1,7 +1,3 @@
-window.addEventListener('deviceorientation', function (data) {
-    console.log(data);
-}, true);
-
 window.addEventListener('orientationchange', function (e) {
     console.log('angle', window.orientation, e);
 }, true);
